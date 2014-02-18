aa290a499839031d8cfb51891424ae95171b602a
Acap72 <andrew@social-media-productions.com>
2014-02-17 22:59:41 -0800

    Basic hibernate changes

---
+++ src/com/stockportfolio/database/HibernateUtilities.java
@@ -0,0 +1,33 @@
+package com.stockportfolio.database;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+public class HibernateUtilities {
+	
+	private static SessionFactory sessionFactory;
+	private static StandardServiceRegistryBuilder serviceRegistry;
+	
+	static
+	{
+		try
+		{
+			Configuration configuration = new Configuration().configure();
+			serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+			sessionFactory = configuration.buildSessionFactory((ServiceRegistry) serviceRegistry);
+			
+		}
+		catch (HibernateException exception)
+		{
+			System.out.println("Problem creating session factory");
+		}
+	}
+public static SessionFactory getSessionFactory()
+{
+	return sessionFactory;
+}
+}
